c80cb9597424a0de8dc7caf2b92da3867ad2e5fa
965579738@qq.com <Long140416>
2017-07-14 16:15:08 +0800

    没改啥

--- app/src/main/java/com/chimu/myapp/MainActivity.java
+++ app/src/main/java/com/chimu/myapp/MainActivity.java
@@ -1,16 +1,12 @@
 package com.chimu.myapp;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.os.Environment;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.annotation.Person;
 import com.example.chimu.myapp.R;
-
-import java.io.FileOutputStream;
 
 @Person(name = "龙文江", age = 35)
 public class MainActivity extends Activity {
@@ -18,27 +14,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        //文通识别调用
-//        Intent intent = new Intent(this,MemoryCameraActivity.class);
-//        startActivityForResult(intent,100);
+
+
+        AssetManager assetManager = getAssets();
+
+
+
+
+
+
+
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 
     //Arouter页面跳转
     private void OnClick() {
-        // 1. 应用内简单的跳转(通过URL跳转在'进阶用法'中)
+//            1. 应用内简单的跳转(通过URL跳转在'进阶用法'中)
 //        ARouter.getInstance().build("/test/ActivityB").navigation();
 
-// 2. 跳转并携带参数
+//        2. 跳转并携带参数
         ARouter.getInstance().build("/test/ActivityB")
                 .withLong("key1", 666L)
                 .withString("key3", "888")
@@ -46,11 +41,5 @@ public class MainActivity extends Activity {
                 .navigation();
     }
 
-    public void save(String name, String content) throws Exception {
-        String url = Environment.getExternalStorageDirectory() + "/logs/";
-        FileOutputStream output = this.openFileOutput(url + name, Context.MODE_PRIVATE);
-        output.write(content.getBytes());
-        output.close();
-    }
 }
 
