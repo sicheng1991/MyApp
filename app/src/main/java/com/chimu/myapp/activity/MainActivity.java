e99fa4eef1763b808f945c3bb94b16aacb2170cd
965579738@qq.com <Long140416>
2017-09-06 17:49:02 +0800

    bitmp任意角圆角

--- app/src/main/java/com/chimu/myapp/activity/MainActivity.java
+++ app/src/main/java/com/chimu/myapp/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -21,6 +23,7 @@ import com.chimu.myapp.R;
 import com.chimu.myapp.common.MessagerService;
 import com.chimu.myapp.common.aidl.LocalService;
 import com.chimu.myapp.common.aidl.RomoteService;
+import com.chimu.mylib.util.BitmapUtil;
 import com.example.annotation.Person;
 
 @Person(name = "龙文江", age = 35)
@@ -37,16 +40,18 @@ public class MainActivity extends BaseActivity {
         LinearLayout ll = (LinearLayout) findViewById(R.id.ll);
        iv = (ImageView) findViewById(R.id.img);
         button = (Button) findViewById(R.id.btn_commit);
+        Bitmap bitmap =  BitmapFactory.decodeResource(this.getResources(),R.mipmap.c1);
+        iv.setImageBitmap(BitmapUtil.fillet(bitmap,10,3));
+
+
+
+
 
 //        双进程拉起
-        Intent i1 = new Intent(this, LocalService.class);
-        startService(i1);
-        Intent i2 = new Intent(this, RomoteService.class);
-        startService(i2);
-
-
-
-//
+//        Intent i1 = new Intent(this, LocalService.class);
+//        startService(i1);
+//        Intent i2 = new Intent(this, RomoteService.class);
+//        startService(i2);
     }
 
     public void OnClick(View v) {
--- mylib/src/main/java/com/chimu/mylib/util/BitmapUtil.java
+++ mylib/src/main/java/com/chimu/mylib/util/BitmapUtil.java
@@ -2,7 +2,13 @@ package com.chimu.mylib.util;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.view.View;
 
 /**
@@ -34,4 +40,83 @@ public class BitmapUtil {
         return bitmap2;
     }
 
+
+
+
+    public static final int CORNER_TOP_LEFT = 1;
+    public static final int CORNER_TOP_RIGHT = 1 << 1;
+    public static final int CORNER_BOTTOM_LEFT = 1 << 2;
+    public static final int CORNER_BOTTOM_RIGHT = 1 << 3;
+    public static final int CORNER_ALL = CORNER_TOP_LEFT | CORNER_TOP_RIGHT | CORNER_BOTTOM_LEFT | CORNER_BOTTOM_RIGHT;
+
+    /**
+     * 图片任意圆角
+     * @param bitmap
+     * @param roundPx  圆角大小
+     * @param corners   左上开始顺顺时针,1+2+4+8
+     * @return
+     */
+    public static Bitmap fillet(Bitmap bitmap, int roundPx,int corners) {
+        try {
+            // 其原理就是：先建立一个与图片大小相同的透明的Bitmap画板
+            // 然后在画板上画出一个想要的形状的区域。
+            final int width = bitmap.getWidth();
+            final int height = bitmap.getHeight();
+
+            Bitmap paintingBoard = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(paintingBoard);
+            canvas.drawARGB(Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT);
+
+            final Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setColor(Color.BLACK);
+
+            //画出4个圆角
+            final RectF rectF = new RectF(0, 0, width, height);
+            canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+            //把不需要的圆角去掉
+            int notRoundedCorners = corners ^ CORNER_ALL;
+            if ((notRoundedCorners & CORNER_TOP_LEFT) != 0) {
+                clipTopLeft(canvas,paint,roundPx,width,height);
+            }
+            if ((notRoundedCorners & CORNER_TOP_RIGHT) != 0) {
+                clipTopRight(canvas, paint, roundPx, width, height);
+            }
+            if ((notRoundedCorners & CORNER_BOTTOM_LEFT) != 0) {
+                clipBottomLeft(canvas,paint,roundPx,width,height);
+            }
+            if ((notRoundedCorners & CORNER_BOTTOM_RIGHT) != 0) {
+                clipBottomRight(canvas, paint, roundPx, width, height);
+            }
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+
+            final Rect src = new Rect(0, 0, width, height);
+            final Rect dst = src;
+            canvas.drawBitmap(bitmap, src, dst, paint);
+            return paintingBoard;
+        } catch (Exception exp) {
+            return bitmap;
+        }
+    }
+
+    private static void clipTopLeft(final Canvas canvas, final Paint paint, int offset, int width, int height) {
+        final Rect block = new Rect(0, 0, offset, offset);
+        canvas.drawRect(block, paint);
+    }
+
+    private static void clipTopRight(final Canvas canvas, final Paint paint, int offset, int width, int height) {
+        final Rect block = new Rect(width - offset, 0, width, offset);
+        canvas.drawRect(block, paint);
+    }
+
+    private static void clipBottomLeft(final Canvas canvas, final Paint paint, int offset, int width, int height) {
+        final Rect block = new Rect(0, height - offset, offset, height);
+        canvas.drawRect(block, paint);
+    }
+
+    private static void clipBottomRight(final Canvas canvas, final Paint paint, int offset, int width, int height) {
+        final Rect block = new Rect(width - offset, height - offset, width, height);
+        canvas.drawRect(block, paint);
+    }
 }
