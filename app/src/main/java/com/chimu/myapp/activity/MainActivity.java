f002dcec553322f3b4a8f37af13dcd5486c9415f
965579738@qq.com <Long140416>
2017-09-12 14:22:55 +0800

    hook

--- app/src/main/java/com/chimu/myapp/activity/MainActivity.java
+++ app/src/main/java/com/chimu/myapp/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.chimu.myapp.activity;
 
+import android.app.Instrumentation;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -20,11 +21,16 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.chimu.myapp.R;
+import com.chimu.myapp.common.EvilInstrumentation;
 import com.chimu.myapp.common.MessagerService;
 import com.chimu.myapp.common.aidl.LocalService;
 import com.chimu.myapp.common.aidl.RomoteService;
 import com.chimu.mylib.util.BitmapUtil;
 import com.example.annotation.Person;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @Person(name = "龙文江", age = 35)
 public class MainActivity extends BaseActivity {
@@ -45,7 +51,7 @@ public class MainActivity extends BaseActivity {
 
 
 
-
+        hook();
 
 //        双进程拉起
 //        Intent i1 = new Intent(this, LocalService.class);
@@ -54,8 +60,45 @@ public class MainActivity extends BaseActivity {
 //        startService(i2);
     }
 
+    private void hook() {
+        // 先获取到当前的ActivityThread对象
+        Class<?> activityThreadClass = null;
+        try {
+            activityThreadClass = Class.forName("android.app.ActivityThread");
+            Method currentActivityThreadMethod = activityThreadClass.getDeclaredMethod("currentActivityThread");
+            currentActivityThreadMethod.setAccessible(true);
+            Object currentActivityThread = currentActivityThreadMethod.invoke(null);
+
+
+            // 拿到原始的 mInstrumentation字段
+            Field mInstrumentationField = activityThreadClass.getField("mInstrumentation");
+            mInstrumentationField.setAccessible(true);
+            Instrumentation mInstrumentation = (Instrumentation) mInstrumentationField.get(currentActivityThread);
+
+            // 创建代理对象
+            Instrumentation evilInstrumentation = new EvilInstrumentation(mInstrumentation);
+
+            // 偷梁换柱
+            mInstrumentationField.set(currentActivityThread, evilInstrumentation);
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public void OnClick(View v) {
-        startAndBindService();
+        startActivity(new Intent(this,TestLockActivity.class));
+//        startAndBindService();
     }
     Intent service;
     private void startAndBindService() {
