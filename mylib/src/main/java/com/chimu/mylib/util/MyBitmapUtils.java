289b56cce69fcfecb80d8f56b81fc30eee13717a
965579738@qq.com <Long140416>
2017-09-15 15:26:29 +0800

    文件util

---
+++ mylib/src/main/java/com/chimu/mylib/util/MyBitmapUtils.java
@@ -0,0 +1,53 @@
+package com.chimu.mylib.util;
+
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+import com.chimu.mylib.R;
+
+/**
+ * Created by Longwj on 2017/9/13.
+ */
+
+public class MyBitmapUtils {
+
+    private NetCacheUtils mNetCacheUtils;
+    private LocalCacheUtils mLocalCacheUtils;
+    private MemoryCacheUtils mMemoryCacheUtils;
+
+    public MyBitmapUtils(){
+        mMemoryCacheUtils=new MemoryCacheUtils();
+        mLocalCacheUtils=new LocalCacheUtils();
+        mNetCacheUtils=new NetCacheUtils(mLocalCacheUtils,mMemoryCacheUtils);
+    }
+
+    /**
+     * 外部调用
+     * @param ivPic
+     * @param url
+     */
+    public void disPlay(ImageView ivPic, String url) {
+//        ivPic.setImageResource(R.styleable.);
+        Bitmap bitmap;
+        //内存缓存
+        bitmap=mMemoryCacheUtils.getBitmapFromMemory(url);
+        if (bitmap!=null){
+            ivPic.setImageBitmap(bitmap);
+            System.out.println("从内存获取图片啦.....");
+            return;
+        }
+
+        //本地缓存
+        bitmap = mLocalCacheUtils.getBitmapFromLocal(url);
+        if(bitmap !=null){
+            ivPic.setImageBitmap(bitmap);
+            System.out.println("从本地获取图片啦.....");
+            //从本地获取图片后,保存至内存中
+            mMemoryCacheUtils.setBitmapToMemory(url,bitmap);
+            return;
+        }
+        //网络缓存
+        mNetCacheUtils.getBitmapFromNet(ivPic,url);
+    }
+}
+
