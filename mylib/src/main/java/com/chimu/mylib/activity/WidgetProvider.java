889bad957f1796e160925d904ef9aa25f012401e
965579738@qq.com <Long140416>
2017-10-24 13:54:17 +0800

    桌面控件

--- mylib/src/main/java/com/chimu/mylib/activity/WidgetProvider.java
+++ mylib/src/main/java/com/chimu/mylib/activity/WidgetProvider.java
@@ -10,73 +10,108 @@ import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.chimu.mylib.R;
+import com.chimu.mylib.service.WidgetService;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by Longwj on 2017/10/24.
  */
 
 public class WidgetProvider extends AppWidgetProvider {
-    public static final String CLICK_ACTION = "com.seewo.appwidgettest.action.CLICK"; // 点击事件的广播ACTION
+    // 更新 widget 的广播对应的action
+    private final String ACTION_UPDATE_ALL = "com.lyl.widget.UPDATE_ALL";
+    // 保存 widget 的id的HashSet，每新建一个 widget 都会为该 widget 分配一个 id。
+    private static Set idsSet = new HashSet();
 
-    /**
-     * 每次窗口小部件被更新都调用一次该方法
-     */
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.app_widget);
-        Intent intent = new Intent(context,MyCameraActivity.class);
-//        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, R.id.btn_reset, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        PendingIntent pi = PendingIntent.getActivity(context, 200, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.btn_reset, pi);
-
-        for (int appWidgetId : appWidgetIds) {
-            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
-        }
-    }
+    public static int mIndex;
 
     /**
      * 接收窗口小部件点击时发送的广播
      */
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         super.onReceive(context, intent);
+        final String action = intent.getAction();
 
-        if (CLICK_ACTION.equals(intent.getAction())) {
-            Toast.makeText(context, "hello dog!", Toast.LENGTH_SHORT).show();
+        if (ACTION_UPDATE_ALL.equals(action)) {
+            // “更新”广播
+            updateAllAppWidgets(context, AppWidgetManager.getInstance(context), idsSet);
+        } else if (intent.hasCategory(Intent.CATEGORY_ALTERNATIVE)) {
+            // “按钮点击”广播
+            mIndex = 0;
+            updateAllAppWidgets(context, AppWidgetManager.getInstance(context), idsSet);
+        }
+    }
+
+    // 更新所有的 widget
+    private void updateAllAppWidgets(Context context, AppWidgetManager appWidgetManager, Set set) {
+        // widget 的id
+        int appID;
+        // 迭代器，用于遍历所有保存的widget的id
+        Iterator it = set.iterator();
+
+        // 要显示的那个数字，每更新一次 + 1
+        mIndex++; // TODO:可以在这里做更多的逻辑操作，比如：数据处理、网络请求等。然后去显示数据
+
+        while (it.hasNext()) {
+            appID = ((Integer) it.next()).intValue();
+
+            // 获取 example_appwidget.xml 对应的RemoteViews
+            RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+
+            // 设置显示数字
+            remoteView.setTextViewText(R.id.tv_1, String.valueOf(mIndex));
+
+            // 设置点击按钮对应的PendingIntent：即点击按钮时，发送广播。
+            remoteView.setOnClickPendingIntent(R.id.btn_reset, getResetPendingIntent(context));
+            remoteView.setOnClickPendingIntent(R.id.btn_open, getOpenPendingIntent(context));
+
+            // 更新 widget
+            appWidgetManager.updateAppWidget(appID, remoteView);
         }
     }
 
     /**
-     * 每删除一次窗口小部件就调用一次
+     * 获取 重置数字的广播
      */
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        super.onDeleted(context, appWidgetIds);
+    private PendingIntent getResetPendingIntent(Context context) {
+        Intent intent = new Intent();
+        intent.setClass(context, WidgetProvider.class);
+        intent.addCategory(Intent.CATEGORY_ALTERNATIVE);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
+        return pi;
     }
 
     /**
-     * 当最后一个该窗口小部件删除时调用该方法
+     * 获取 打开 MainActivity 的 PendingIntent
      */
-    @Override
-    public void onDisabled(Context context) {
-        super.onDisabled(context);
+    private PendingIntent getOpenPendingIntent(Context context) {
+        Intent intent = new Intent();
+        intent.setClass(context, MyCameraActivity.class);
+        intent.putExtra("main", "这句话是我从桌面点开传过去的。");
+        PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
+        return pi;
     }
 
     /**
-     * 当该窗口小部件第一次添加到桌面时调用该方法
+     * 当该窗口小部件第一次添加到桌面时调用该方法，可添加多次但只第一次调用
      */
     @Override
     public void onEnabled(Context context) {
+        // 在第一个 widget 被创建时，开启服务
+        Intent intent = new Intent(context, WidgetService.class);
+        context.startService(intent);
+        Toast.makeText(context, "开始计数", Toast.LENGTH_SHORT).show();
         super.onEnabled(context);
     }
 
-    /**
-     * 当小部件大小改变时
-     */
+    // 当 widget 被初次添加 或者 当 widget 的大小被改变时，被调用
     @Override
-    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle
+            newOptions) {
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
     }
 
@@ -87,5 +122,42 @@ public class WidgetProvider extends AppWidgetProvider {
     public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
         super.onRestored(context, oldWidgetIds, newWidgetIds);
     }
+
+    /**
+     * 每次窗口小部件被点击更新都调用一次该方法
+     */
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        // 每次 widget 被创建时，对应的将widget的id添加到set中
+        for (int appWidgetId : appWidgetIds) {
+            idsSet.add(Integer.valueOf(appWidgetId));
+        }
+    }
+
+    /**
+     * 每删除一次窗口小部件就调用一次
+     */
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // 当 widget 被删除时，对应的删除set中保存的widget的id
+        for (int appWidgetId : appWidgetIds) {
+            idsSet.remove(Integer.valueOf(appWidgetId));
+        }
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    /**
+     * 当最后一个该窗口小部件删除时调用该方法，注意是最后一个
+     */
+    @Override
+    public void onDisabled(Context context) {
+        // 在最后一个 widget 被删除时，终止服务
+        Intent intent = new Intent(context, WidgetService.class);
+        context.stopService(intent);
+        super.onDisabled(context);
+    }
+
+
 
 }
