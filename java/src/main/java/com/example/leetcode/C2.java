9beab628b053de1813b0e30a9ce245fe04d63d05
yangzteL <965579738@qq.com>
2018-05-03 13:51:24 +0800

    C2,C3

---
+++ java/src/main/java/com/example/leetcode/C2.java
@@ -0,0 +1,47 @@
+package com.example.leetcode;
+
+/**
+ * 给您两个非空链表，表示两个非负整数。这些数字以相反的顺序存储，每个节点都包含一个数字。将两个数字相加，并将其作为链表返回。
+ * Created by yangzteL on 2018/5/3 0003.
+ */
+
+public class C2 {
+    public static void main(String [] ar){
+        ListNode l1 = new ListNode(5);
+//        l1.next = new ListNode(4);
+//        l1.next.next = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+//        l2.next = new ListNode(6);
+//        l2.next.next = new ListNode(4);
+
+        addTwoNumbers(l1,l2);
+    }
+    public  static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int add = 0;
+        ListNode node = new ListNode(0);
+        ListNode node1 = node;
+        while(l1 != null || l2 != null){
+            int v1 = l1 != null ? l1.val:0;
+            int v2 = l2 != null ? l2.val:0;
+            node.next = new ListNode((v1 + v2 + add) % 10);
+            add = (v1 + v2 + add) / 10;
+            node = node.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if(add > 0){
+            node.next = new ListNode(add);
+        }
+        return node1.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
---
+++ java/src/main/java/com/example/leetcode/C3.java
@@ -0,0 +1,37 @@
+package com.example.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 最长的子字符串，没有重复字符
+ * <p>
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * <p>
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * <p>
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ * <p>
+ * Created by yangzteL on 2018/5/3 0003.
+ */
+
+public class C3 {
+
+    public static void main(String[] args){
+            lengthOfLongestSubstring("abcba");
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length(), max = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            char c = s.charAt(j);
+            if (map.containsKey(c)) {
+                i = Math.max(map.get(c), i);
+            }
+            max = Math.max(max, j - i + 1);
+            map.put(c, j + 1);
+        }
+        return max;
+    }
+}
