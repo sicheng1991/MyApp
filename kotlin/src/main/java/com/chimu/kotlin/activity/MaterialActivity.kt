abd9f461f7bdd3b27e9f088a9c5f7a7a246058dc
yangzteL <965579738@qq.com>
2018-12-28 08:57:22 +0800

    haha

--- kotlin/src/main/java/com/chimu/kotlin/activity/MaterialActivity.kt
+++ kotlin/src/main/java/com/chimu/kotlin/activity/MaterialActivity.kt
@@ -2,16 +2,12 @@ package com.chimu.kotlin.activity
 
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
-import android.view.View
 import android.view.Window
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.chimu.kotlin.R
-import com.chimu.kotlin.R.id.bar
 import com.google.android.material.bottomappbar.BottomAppBar
-import com.chimu.kotlin.R.id.bar
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.chimu.kotlin.R.id.bar
+
 
 
 
@@ -31,20 +27,26 @@ class MaterialActivity : BActivity() {
 //                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 //                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
 
-        val bar: BottomAppBar = findViewById(R.id.bar)
+        val bar= findViewById<BottomAppBar>(R.id.bar)
+
+
+        bar.setOnMenuItemClickListener({ item ->
+            Toast.makeText(this, item.itemId, Toast.LENGTH_LONG).show()
+            true
+        }
+
+        )
 
         //
         bar.setOnMenuItemClickListener {
             true
         }
 
+//        bar.replaceMenu(R.menu.bottom_app_bar)
         bar.setNavigationOnClickListener {
             // Handle the navigation click by showing a BottomDrawer etc.
             run {
                 Log.i("haa", "123")
-                Log.i("haa", "233")
-
-
 
             }
         }
